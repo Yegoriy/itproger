2b1a88e3a095667dadfa163938c8138a5702f8f3
Yegoriy <YegorShaytarov@gmail.com>
2024-04-02 23:34:26 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Мой проект на питоне
+# print("Hello itProgger")
+# number_value = 1000
+# print(5, "Нах", sep = "_", end = "~")
+# print(6, "Нах", sep = "_", end = "\t")
+# print(7, "Нах", sep = "_", end = "~")
+# number_value = int(input("Введите любое число : "))
+# if number_value > 100:
+#    print("Вы ввели число больше 100")
+#    number_value = "перевод в стринг"
+# elif True:
+#    print("Вы ввели число меньше 100")
+#    number_value = 10.1
+
+# for i in "Хрен вам":
+#    if i == "р":
+#        found = True
+#        break
+#    else:
+#        found = False
+# print(found)
+
+list_val = [1, 2, 3, 4]
+
+add_val = [5, 6]
+
+list_val.extend(add_val)
+
+print(list_val)
